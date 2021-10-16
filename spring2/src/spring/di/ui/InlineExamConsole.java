9ad121aa32bd8318d0ad50cd76f250b7a8f9c4bc
leejunghwan <coled@naver.com>
2021-10-16 22:25:19 +0900

    10-16 공부 내용 추가 14강 완강

--- spring2/src/spring/di/ui/InlineExamConsole.java
+++ spring2/src/spring/di/ui/InlineExamConsole.java
@@ -6,30 +6,43 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
-
-	
+	//required = false 
+	//객체가 일단 없어도 OK 진행하여라  14강까지 내용 
+	@Autowired(required = false)
+	@Qualifier("exam2")
 	private Exam exam;
-	
+	//프라이빗인데 어떻게? 기본생성자가 생성되면서 된다.
 	public InlineExamConsole() {
 		System.out.println("constructor");
 	}
 	
+	
+	
 	public InlineExamConsole(Exam exam) {		
-		this.exam = exam;
 		System.out.println("over constructor");
-
+		this.exam = exam; 
+		
 	}
-
+	/*
+	 * @Autowired public InlineExamConsole(
+	 * 
+	 * @Qualifier("exam1")Exam exam) { System.out.println("over constructor");
+	 * this.exam = exam;
+	 * 
+	 * }
+	 */
 
 	@Override
 	public void print() {
-		System.out.println("setter");
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		if (exam == null) {
+			System.out.printf("total is %d, avg is %f\n", 0,0.0);
+		}else {
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		}
 	}
 
 
-	@Autowired
-	@Qualifier("exam1")
+
 	@Override
 	public void setExam(Exam exam) {
 		this.exam = exam;
