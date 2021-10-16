22fb86c8136e1dbabe7c46b88b2b2c4499251548
leejunghwan <coled@naver.com>
2021-10-16 21:18:13 +0900

    10-16 @Autowired

--- spring2/src/spring/di/ui/InlineExamConsole.java
+++ spring2/src/spring/di/ui/InlineExamConsole.java
@@ -1,5 +1,8 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
@@ -8,21 +11,25 @@ public class InlineExamConsole implements ExamConsole {
 	private Exam exam;
 	
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		System.out.println("constructor");
 	}
 	
 	public InlineExamConsole(Exam exam) {		
 		this.exam = exam;
+		System.out.println("over constructor");
+
 	}
 
 
 	@Override
 	public void print() {
+		System.out.println("setter");
 		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 	}
 
 
-
+	@Autowired
+	@Qualifier("exam1")
 	@Override
 	public void setExam(Exam exam) {
 		this.exam = exam;
